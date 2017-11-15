df40e9ab034ad66e969335dcc675005263ba7dd1
Tony Xu <xuology@gmail.com>
2017-11-15 10:14:21 -0500

    ng init and node server initialization

---
+++ server/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+
+app.use(express.static(path.join(__dirname, '/../dist')));
+app.use(express.static(path.join(__dirname, '/lib')));
+
+// require('./routes/routes.js')(app, express);
+
+const port = process.env.PORT || 8000;
+
+module.exports = app.listen(port, function() {
+  console.log('Listening on port ', port);
+});
