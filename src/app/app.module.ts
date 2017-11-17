2f069bec6ab667a35b763aed667a127e372a1472
Tony Xu <xuology@gmail.com>
2017-11-17 12:42:34 -0500

    add google maps as background to landing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AgmCoreModule } from '@agm/core';
 
 import { SearchModule } from './search/search.module';
 import { ResultsModule } from './results/results.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { LandingComponent } from './landing/landing.component';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
     AppRoutingModule,
     SearchModule,
-    ResultsModule
+    ResultsModule,
+    AgmCoreModule.forRoot({
+      apiKey: environment.PLACES_API_KEY
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+  title: string = 'Background Map';
+  lat: number = 41.0157;
+  lng: number = -74.20911;
+  zoom: number = 12;
 
   constructor() { }
 
