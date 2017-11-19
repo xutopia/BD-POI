01f39aaa2d9df856ef20e59d572f405c49469944
Tony Xu <xuology@gmail.com>
2017-11-18 22:59:57 -0500

    add responsive navbar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  collapse: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+    // toggleState() {
+    //     let bool = this.isIn;
+    //     this.isIn = bool === false ? true : false;
+    // }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -7,6 +8,7 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   imports: [
     CommonModule,
+    NgbModule,
   ],
   declarations: [
     HeaderComponent,
